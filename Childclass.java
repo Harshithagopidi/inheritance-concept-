581ec909fcec0b3117e41106417d9d1b2281708e
Harshithagopidi <harshithareddygopidi3010@gmail.com>
2023-09-14 19:51:59 -0700

    inheritance command

---
+++ Childclass.java
@@ -0,0 +1,33 @@
+package practice;
+
+public class Childclass extends Praentclass {
+
+	public static void main(String[] args) {
+
+		cc();
+		rpm();
+		gear();
+		engine();
+		getColor();
+
+	}
+	
+	public static void cc() {
+		System.out.println(" cc implemented");
+	}
+	
+	public static void rpm() {
+		System.out.println(" rpm implemented");
+	}
+	
+	public static void getColor() {
+		System.out.println(color);
+	}
+	
+	//public static void engine() {
+		//System.out.println("engine method is implemeneted in child");
+	
+
+	}
+
+
---
+++ Praentclass.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class Praentclass {
+	static String color = "Red";
+
+	public static void main(String[] args) {
+		
+			
+			gear();
+			engine();
+
+		}
+		
+		public static void gear() {
+			
+			System.out.println("Gear method is implemeneted");
+		}
+		
+		public static void engine() {
+			
+			System.out.println("engine method is implemeneted in parent");
+		}
+		
+
+	}
+
+
